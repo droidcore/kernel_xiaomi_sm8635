e15d707a777a86e6849066d11d3928501fb03e3b
droidcore <agusmahardika99@gmail.com>
2025-10-19 04:17:56 +0700

    Rename kernel from Chidori to BLUryznstk

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Chidori-Kernel
+kernel.string=BLUryznstk-Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -30,7 +30,7 @@ no_magisk_check=1
 
 kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
 
-ui_print "Flashing Chidori Kernel..."
+ui_print "Flashing BLUryzn Kernel..."
 ui_print "Kernel version detected:".$kernel_version
 
 # boot install
